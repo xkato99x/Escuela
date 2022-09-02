2dcfe809e005e53935427c640b4614e63ffce077
xkato99x <73802717+xkato99x@users.noreply.github.com>
2022-09-01 20:38:02 -0500

    Add files via upload

--- Explorar datos de peliculas en R.r
+++ Explorar datos de peliculas en R.r
@@ -6,14 +6,16 @@ library(fmsb)
 datos <- read.csv("https://raw.githubusercontent.com/rpizarrog/Analisis-Inteligente-de-datos/main/datos/movies-db.csv", stringsAsFactors = TRUE )
 #print(datos)
 
-sortRtg <- datos[order(datos$average_rating), ] %>% summarise(average_rating) 
-sortRtg$l <- datos %>% select(1,6)
+sortRtg <- datos[order(datos$average_rating), ] %>% select(1,2,5,6,7)
+# sortRtg <- datos[order(datos$average_rating), ] %>% summarise(average_rating) 
 print(sortRtg)
 ggplot(data = sortRtg) + geom_bar(aes(x = average_rating))
+# Con este sorting podemos observar que las 5 películas mejor rateadas son domésticas, siendo la mejor
+# rateada Jumanji (2020), con una calificación de 9.3 y 50 millones de presupuesto.
 
-sortCost <- datos[order(datos$cost_millions), ] %>% summarise(cost_millions) 
-sortCost$l <- datos %>% select(1, 5)
+sortCost <- datos[order(datos$cost_millions), ] %>% select(1, 5, 6)
 print(sortCost)
+# Aquí podemos observar que 
 
 sortGenre <- datos %>% select(4,6) %>% group_by(genre) %>% summarise(frecuencia  = n())
 print(sortGenre)
